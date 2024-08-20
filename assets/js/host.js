5b03f35659154a1f005d3ee197aeca9918ac2650
Саша Виндман <alvin@MacBook-Pro-Sasa.local>
2024-08-20 03:32:13 +0300

    Edited frens page.

--- assets/js/host.js
+++
@@ -1,6 +0,0 @@
-fetch('/api/get-telegram-user')
-    .then(response => response.json())
-    .then(data => {
-        const userName = data.username; // Полученное имя пользователя из Telegram
-        document.querySelector('#username').textContent = userName;
-    });
